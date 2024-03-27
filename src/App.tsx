08f392d32151dbd62fc53ff257a2d993df623d20
agnkos <101097155+agnkos@users.noreply.github.com>
2024-03-27 12:10:50 +0100

    rowsPerPage - select

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { Box, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
+import { indigo } from '@mui/material/colors'
 import axios from "axios"
 import TagsTable from "./components/TagsTable"
 
@@ -9,7 +10,7 @@ function App() {
   const [tags, setTags] = useState([])
 
   useEffect(() => {
-    axios.get(`https://api.stackexchange.com/2.3/tags?site=stackoverflow&pagesize=100&key=${import.meta.env.STACKEXCHANGE_API_KEY}`)
+    axios.get(`https://api.stackexchange.com/2.3/tags?site=stackoverflow&pagesize=100&key=${import.meta.env.VITE_STACKEXCHANGE_API_KEY}`)
       .then(response => {
         console.log(response.data)
         setTags(response.data.items)
@@ -17,9 +18,13 @@ function App() {
       .catch(error => console.log(error))
   }, [])
 
+  useEffect(() => {
+    console.log('tags', tags)
+  }, [tags])
+
   return (
     <Box p={4}>
-      <Typography variant="h3" component="h1" sx={{ marginBottom: "1rem", fontWeight: "bold" }}>
+      <Typography variant="h3" component="h1" sx={{ marginBottom: "1rem", fontWeight: "bold", color: indigo[600] }}>
         Tags App
       </Typography>
       <TagsTable tags={tags} />
